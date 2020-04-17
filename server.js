9ce067c69cf19c4b771d76bba8c02a24acb33e26
sreejith <sreejithag97@gmail.com>
2020-04-17 13:07:21 +0530

    errors fixed

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/rate-convertor'));
+app.use(express.static('./dist/RateConvertor'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/rate-convertor/'}),
+    res.sendFile('index.html', {root: 'dist/RateConvertor/'}),
 );
 
 app.listen(process.env.PORT || 8080);
